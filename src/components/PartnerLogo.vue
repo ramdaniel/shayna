42a404f9079c2e9ca3000b8489ca720fc36d1b2c
ramdaniel <daniel.aryas7@gmail.com>
2022-03-21 18:38:45 +0700

    upload projec vue

---
+++ src/components/PartnerLogo.vue
@@ -0,0 +1,29 @@
+<template>
+   <!-- Partner Logo Section Begin -->
+    <section class="partner-logo">
+        <div class="container">
+            <carousel class="logo-carousel" :loop="true" :autoplay="true" :items="5" :dots="false" :nav="false">
+                <div class="logo-item">
+                    <div class="tablecell-inner">
+                        <img src="img/logo-carousel/logo-1.png" alt="" />
+                    </div>
+                </div>
+                
+            </carousel>
+                    </div>
+    </section>
+    <!-- Partner Logo Section End -->
+</template>
+
+<script>
+import carousel from 'vue-owl-carousel'
+export default {
+    name:'PartnerLogo',
+    component: {
+        carousel
+    }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+     <NavbarHeader/>
+   <OwlCarousel/>
+   <SliderBawah/>
+   <InstaSlide/>
+   <PartnerLogo/>
+   <FooterGa/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavbarHeader from '@/components/NavbarHeader.vue';
+import OwlCarousel from '@/components/OwlCarousel.vue';
+import SliderBawah from '@/components/SliderBawah.vue';
+import InstaSlide from '@/components/InstaSlide.vue';
+import PartnerLogo from '@/components/PartnerLogo.vue';
+import FooterGa from '@/components/FooterGa.vue';
+export default {
+  name: 'HomeView',
+  components: {
+    NavbarHeader,
+    OwlCarousel,
+    SliderBawah,
+    InstaSlide,
+    PartnerLogo,
+    FooterGa
+  }
+}
+</script>
